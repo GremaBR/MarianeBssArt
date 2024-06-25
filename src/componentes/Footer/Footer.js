f8e7d78c4cf1871d54d63fb600fa90653f191a22
Flavia Schnaider <schflavia@hotmail.com>
2024-06-25 16:20:34 -0300

    Ultimo de agora

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -9,40 +9,42 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function Footer() {
   return (
     <footer>
-      <div className="footer-items">
-        <div className="esquerda">
-          <p className="mariane-footer">
-            Mariane BSS Art
-          </p>
-          <p>
-            “Criatividade é conectar o que parece desconectado.”<br />
-           -  Willian Plomer
-          </p>
-        </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="mariane-footer display-flex">
+              Mariane BSS Art
+            </p>
+            <p className="display-flex">
+              “Criatividade é conectar o que parece desconectado.”<br />
+              -  Willian Plomer
+            </p>
+          </div>
 
-        <div className="direita">
-          <p className="contatos"> CONTATOS </p>
-          <hr />
+          <div class="col-md-6 ">
+            <p class="contatos"> CONTATOS </p>
+            <hr className="hrFooter"/>
 
-          <a className="footer-link" href="#">
-            <img src={iconFone} alt="Fone Icon" />
-            +55 (47) 988719165
-          </a>
+            <a class="footer-link" href="#">
+              <img src={iconFone} alt="Fone Icon" />
+              +55 (47) 988719165
+            </a>
 
-          <a className="footer-link" href="#">
-            <img src={iconEmail} alt="Email Icon" />
-            marianebasseggio@gmail.com
-          </a>
+            <a class="footer-link" href="#">
+              <img src={iconEmail} alt="Email Icon" />
+              marianebasseggio@gmail.com
+            </a>
 
-          <a className="footer-link" href="#">
-            <img src={iconEndereco} alt="Endereco Icon" />
-            Balneário Camboriú, SC, Brasil
-          </a>
+            <a class="footer-link" href="#">
+              <img src={iconEndereco} alt="Endereco Icon" />
+              Balneário Camboriú, SC, Brasil
+            </a>
 
-          <a className="footer-link" href="https://www.instagram.com/mariane_bss_art/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D" target="_blank">
-            <img src={iconInstagram} alt="Instagram Icon" />
-            @mariane_bss_art
-          </a>
+            <a class="footer-link" href="https://www.instagram.com/mariane_bss_art/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D" target="_blank">
+              <img src={iconInstagram} alt="Instagram Icon" />
+              @mariane_bss_art
+            </a>
+          </div>
         </div>
       </div>
     </footer>
--- src/componentes/Home/Home.js
+++ src/componentes/Home/Home.js
@@ -2,20 +2,17 @@ import './Home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HrHome from '../../img/Rectangle 37.png';
-import HomeImg from '../../img/home.png';
+import HomeImg from '../../img/Group 25.png';
+
 function Home() {
     return (
-        <main>
-            <div class="corpo">
-                <img src={HomeImg} class="float-left"/>
-                    <div class="frase">
-                        <img src={HrHome} class="hr-frase" />
-                        <p class="p-frase"> "Somos telas em branco e podemos ser arte."</p>
-                        <p class="autor"> - Femingos</p>
-                    </div>
+        <main className="container">
+            <div className="row align-items-center">
+                <div className="col-12">
+                    <img src={HomeImg} className="img-fluid" alt="Home" />
+                </div>
             </div>
         </main>
     );
 }
-
 export default Home;
