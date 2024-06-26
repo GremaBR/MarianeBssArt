e435bd683566043c9ac6d294506bed6c5f009a09
Grema <gremabr@gmail.com>
2024-06-26 17:51:31 -0300

    Header Update

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -16,66 +16,67 @@ function Header() {
         <header>
             <nav class="navbar bg-body-tertiary top">
                 <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                    <div class="header">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                    <Link to="/"><img src={Assinatura} class="img-header"/></Link>
+                        <Link to="/"><img src={Assinatura} class="img-header" /></Link>
 
                         <form class="d-flex" role="search">
                             <button class="btn btn-outline-success" type="submit">Entre em contato pelo WhatsApp</button>
                         </form>
-
-                        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                            <div class="offcanvas-header">
-                                <img src={Assinatura} class="logo-sidebar"/>
-                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page">Principais</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <Link class="nav-link item" to="/">
-                                            <img src={IconCasa}/>
-                                                Home
-                                        </Link>
-                                        <Link class="nav-link item" to="/about">
-                                            <img src={IconMulher}/>
-                                                Sobre mim
-                                        </Link>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page">Artes</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <Link class="nav-link item" to="/aquarela">
-                                            <img src={IconAquarela} />
-                                                Aquarela
-                                        </Link>
-                                        <Link class="nav-link item" to="/tela">
-                                            <img src={IconTelaDePintura} />
-                                                Telas
-                                        </Link>
-                                        <Link class="nav-link item" to="/grafite">
-                                            <img src={IconLapis} />
-                                                Grafite
-                                        </Link>
-                                        <Link class="nav-link item" to="/disponiveis">
-                                            <img src={IconCompra} />
-                                                Artes Disponíveis
-                                        </Link>
-                                    </li>
-
-                                </ul>
-                                <form class="d-flex" role="search">
-                                    <button class="btn btn-outline-success" type="submit">Encomende sua arte</button>
-                                </form>
-                            </div>
+                    </div>
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <img src={Assinatura} class="logo-sidebar" />
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
+
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page">Principais</a>
+                                </li>
+                                <li class="nav-item">
+                                    <Link class="nav-link item" to="/">
+                                        <img src={IconCasa} />
+                                        Home
+                                    </Link>
+                                    <Link class="nav-link item" to="/about">
+                                        <img src={IconMulher} />
+                                        Sobre mim
+                                    </Link>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page">Artes</a>
+                                </li>
+                                <li class="nav-item">
+                                    <Link class="nav-link item" to="/aquarela">
+                                        <img src={IconAquarela} />
+                                        Aquarela
+                                    </Link>
+                                    <Link class="nav-link item" to="/tela">
+                                        <img src={IconTelaDePintura} />
+                                        Telas
+                                    </Link>
+                                    <Link class="nav-link item" to="/grafite">
+                                        <img src={IconLapis} />
+                                        Grafite
+                                    </Link>
+                                    <Link class="nav-link item" to="/disponiveis">
+                                        <img src={IconCompra} />
+                                        Artes Disponíveis
+                                    </Link>
+                                </li>
+
+                            </ul>
+                            <form class="d-flex" role="search">
+                                <button class="btn btn-outline-success" type="submit">Encomende sua arte</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
